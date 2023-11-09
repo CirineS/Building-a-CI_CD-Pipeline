c1cddb5663de2b7c0c8ce61317e330ff5979720f
CirineS <142796680+CirineS@users.noreply.github.com>
2023-11-09 20:54:31 +0100

    Update loadtesting.sh

--- loadtesting.sh
+++ loadtesting.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-locust -f locustfile.py --host https://mywebapp345678.azurewebsites.net/ --users 100 --spawn-rate 5
+locust -f locustfile.py --host https://mywebapp345678.azurewebsites.net/ --users 100 --spawn-rate 5 --web-port 8089
